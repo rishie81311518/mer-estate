c760c95bf32e497739eb5646f233084180ed4d89
Rishi <rishiekandala@gmail.com>
2024-02-22 20:30:34 +0530

    Update header and make profile page private

---
+++ api/models/user.model.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    avatar: {
+      type: String,
+      default:"https://t4.ftcdn.net/jpg/03/64/21/11/360_F_364211147_1qgLVxv1Tcq0Ohz3FawUfrtONzz8nq3e.jpg"
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
---
+++ client/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+projectId: "mern-estate-a9e75",
+authDomain: "mern-estate-a9e75.firebaseapp.com",
+storageBucket: "mern-estate-a9e75.appspot.com",
+messagingSenderId: "354077010609",
+appId: "1:354077010609:web:deb8df22ccfbbc2a7a1090"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
