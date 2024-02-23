4ad676eacb213d15dcef694f7461b65f15431fe2
Rishi <rishiekandala@gmail.com>
2024-02-23 10:48:15 +0530

    create user update api

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import express from "express";
 import mongoose from "mongoose";
 import authRouter from "./routes/auth.route.js";
 import userRouter from "./routes/user.route.js";
+import cookieParser from "cookie-parser";
 dotenv.config();
 
 mongoose
@@ -16,6 +17,8 @@ mongoose
 const app = express();
 
 app.use(express.json());
+
+app.use(cookieParser());
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
