8f215a2b4bcde29dba61e2a2b23471b141e5796e
Rishi <rishiekandala@gmail.com>
2024-03-09 17:49:45 +0530

    complete home page

--- client/src/components/ListingItem.jsx
+++ client/src/components/ListingItem.jsx
@@ -4,7 +4,7 @@ import { MdLocationOn } from "react-icons/md";
 function ListingItem({ listing }) {
   return (
     <div className="bg-white shadow-md hover:shadow-lg transition-shadow overflow-hidden rounded-lg w-full sm:w-[330px]">
-      <Link to={`listing/${listing._id}`}>
+      <Link to={`/listing/${listing._id}`}>
         <img
           src={listing.imageUrls[0] ||'https://img.freepik.com/free-photo/analog-landscape-city-with-buildings_23-2149661456.jpg?size=626&ext=jpg' }
           alt="listing cover"
